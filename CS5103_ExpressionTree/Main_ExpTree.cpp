56b63083ffcb2821e3a1d1958abd750409daaa74
DJ Yuhn <djyuhn@gmail.com>
2017-11-09 21:19:40 -0600

    Create a tree of the reverse polish notation expression. Create functions to allow for preorder, inorder, and postorder traversal of tree.

--- CS5103_ExpressionTree/Main_ExpTree.cpp
+++ CS5103_ExpressionTree/Main_ExpTree.cpp
@@ -1,7 +1,7 @@
 /**
-CS5103 - Assignment 2 Expression Tree
+	CS5103 - Assignment 2 Expression Tree
 
-@author DJ Yuhn
+	@author DJ Yuhn
 */
 
 #include <iostream>
@@ -10,27 +10,107 @@ CS5103 - Assignment 2 Expression Tree
 #include <map>
 #include <list>
 #include <stack>
-#include<algorithm>
+#include <algorithm>
+
+struct node {
+	char data;
+	node* left = nullptr;
+	node* right = nullptr;
+};
 
 bool isOperator(char op);
+void preorder(node * leaf);
+void inorder(node * leaf);
+void postorder(node * leaf);
+std::string shuntingYard(std::string rpnExpr);
 
 int main() {
-	std::vector<char> output;
-	std::stack<char> stack;
 	std::string expr = "(a + b * c) + ((d * e + f) * g)";
 
-	// Remove whitespace from the expression
-	expr.erase(std::remove_if(expr.begin(), expr.end(), isspace), expr.end());
+	std::stack<node*> treeStack;
+
+	std::string rpnExpr = shuntingYard(expr);
+
+	for (int i = 0; i < rpnExpr.size(); i++) {
+		if (!isOperator(rpnExpr[i])) {
+			node *newNode = new node;
+			newNode->data = rpnExpr[i];
+			treeStack.push(newNode);
+		}
+
+		else if (isOperator(rpnExpr[i])) {
+			node *newNode = new node;
+			newNode->data = rpnExpr[i];
+			newNode->right = treeStack.top();
+			treeStack.pop();
+			newNode->left = treeStack.top();
+			treeStack.pop();
+			treeStack.push(newNode);
+		}
+	}
 	
+	std::cout << "Original Expression:\n" << expr 
+		<< "\nParentheses are removed in the output below.\n\n";
+	std::cout << "Preorder:" << std::endl;
+	preorder(treeStack.top());
+	std::cout << "\n\nInorder:" << std::endl;
+	inorder(treeStack.top());
+	std::cout << "\n\nPostorder:" << std::endl;
+	postorder(treeStack.top());
+	std::cout << "\n\n";
+
+	return 0;
+}
+
+bool isOperator(char op) {
+	if (op == '+' || op == '-' ||
+		op == '*' || op == '/')
+		return true;
+	else
+		return false;
+}
+
+void preorder(node * leaf) {
+	if (leaf != nullptr) {
+		std::cout << leaf->data << " ";
+		preorder(leaf->left);
+		preorder(leaf->right);
+	}
+};
+
+void inorder(node * leaf) {
+	if (leaf != nullptr) {
+		inorder(leaf->left);
+		std::cout << leaf->data << " ";
+		inorder(leaf->right);
+	}
+};
+
+void postorder(node * leaf) {
+	if (leaf != nullptr) {
+		postorder(leaf->left);
+		postorder(leaf->right);
+		std::cout << leaf->data << " ";
+	}
+};
+
+std::string shuntingYard(std::string expr) {
+	std::vector<char> output;
+	std::stack<char> stack;
+	std::string outputString;
+
 	std::map<char, int> opPriority;
 	opPriority['+'] = 10;
 	opPriority['-'] - 10;
 	opPriority['*'] = 20;
 	opPriority['/'] - 20;
 
+	// Remove whitespace from the expression
+	expr.erase(std::remove_if(expr.begin(), expr.end(), isspace), expr.end());
+
 	// Gather the characters
 	for (int i = 0; i < expr.size(); i++) {
-		
+
 		// If character is operator
 		if (isOperator(expr[i])) {
 			char o1 = expr[i];
@@ -90,19 +170,8 @@ int main() {
 	}
 
 	for (int i = 0; i < output.size(); i++)
-		std::cout << output[i];
+		outputString += output[i];
 
+	return outputString;
 
-	system("pause");
-
-
-	return 0;
-}
-
-bool isOperator(char op) {
-	if (op == '+' || op == '-' ||
-		op == '*' || op == '/')
-		return true;
-	else
-		return false;
 }
