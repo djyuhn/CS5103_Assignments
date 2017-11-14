ff42a5040ba75bfd1f5d59d3c785a3a38b8cdccb
DJ Yuhn <djyuhn@gmail.com>
2017-11-14 11:19:50 -0600

    Declare methods within AVL class.
    Need to define methods.

--- CS5103_AVLTree/AVL.h
+++ CS5103_AVLTree/AVL.h
@@ -13,11 +13,13 @@ public:
 	AVL() { root = nullptr; }
 	~AVL();
 
+	void insert(T val) { insert(val, root); }
+
 private:
 	struct node {
 		T data;
-		node *left;
-		node *right;
+		node* left;
+		node* right;
 		int height;
 
 		node(T data) {
@@ -26,9 +28,21 @@ private:
 			right = nullptr;
 			height = 0;
 		}
+
+		void updateHeight();
 	};
 
-	node *root;
+	node* root;
+
+	void insert(T data, node* leaf);
+	void destroyTree(node*& leaf);
+
+	int getDiff(node* leaf);
+	node* rotateLeft(node*& leaf);
+	node* rotateRight(node*& leaf);
+	node* rotateRightLeft(node*& leaf);
+	node* rotateLeftRight(node*& leaf);
+	void rebalance(node*& leaf);
 };
 
 #endif // !AVL_H
